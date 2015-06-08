506d3a5f0e9691a3f1abf9133cb5d77deb5bb66d
Stanislas Chevallier <contact@stanislaschevallier.fr>
2015-06-08 13:40:42 +0200

    Initial Commit

---
+++ DisableDigitalOut/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DisableDigitalOut
+//
+//  Created by Stanislas Chevallier on 08/06/15.
+//  Copyright (c) 2015 Syan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DisableDigitalOut/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DisableDigitalOut
+//
+//  Created by Stanislas Chevallier on 08/06/15.
+//  Copyright (c) 2015 Syan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
